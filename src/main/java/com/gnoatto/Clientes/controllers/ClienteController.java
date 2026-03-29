1266eab908400c1e16e73e48e3b90b17861c0063
Gabriel Gnoatto <gnoattogabriel100@gmail.com>
2026-03-29 18:12:11 -0300

    restful

--- src/main/java/com/gnoatto/Clientes/controllers/ClienteController.java
+++ src/main/java/com/gnoatto/Clientes/controllers/ClienteController.java
@@ -4,6 +4,7 @@ package com.gnoatto.Clientes.controllers;
 import com.gnoatto.Clientes.models.ClienteModel;
 import com.gnoatto.Clientes.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,28 +18,33 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @PostMapping
-    public ClienteModel criarCliente(@RequestBody ClienteModel novoCliente){
-        return clienteService.criarCliente(novoCliente);
+    public ResponseEntity<ClienteModel> criarCliente(@RequestBody ClienteModel novoCliente){
+        ClienteModel cliente = clienteService.criarCliente(novoCliente);
+        return ResponseEntity.status(201).body(cliente);
     }
 
     @GetMapping
-    public List<ClienteModel> buscarTodos(){
-        return clienteService.listarTodos();
+    public ResponseEntity<List<ClienteModel>> buscarTodos(){
+        return ResponseEntity.ok(clienteService.listarTodos());
     }
 
     @DeleteMapping("/{id}")
-    public void deletarCliente(@PathVariable Long id){
+    public ResponseEntity<?> deletarCliente(@PathVariable Long id){
         clienteService.deletarCliente(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
-    public Optional<ClienteModel> buscarPorId(@PathVariable Long id){
-        return clienteService.buscarPorId(id);
+    public ResponseEntity<ClienteModel> buscarPorId(@PathVariable Long id){
+        return clienteService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public ClienteModel atualizarCliente(@PathVariable Long id,@RequestBody ClienteModel novoCliente){
-        return clienteService.atualizarCliente(id, novoCliente);
+    public ResponseEntity<ClienteModel> atualizarCliente(@PathVariable Long id,@RequestBody ClienteModel novoCliente){
+        ClienteModel cliente = clienteService.atualizarCliente(id, novoCliente);
+        return ResponseEntity.ok(cliente);
     }
 
 }
